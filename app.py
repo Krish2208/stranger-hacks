dc181878eb865d9922dd987341995b72d11c04a6
Krish Agrawal <diwankrish.17@gmail.com>
2023-04-03 01:25:39 +0530

    final

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 from flask import Flask, render_template, request, url_for, redirect
-
+import wave
 from gtts import gTTS
-import requests
 from PIL import Image
 from transformers import BlipProcessor, BlipForConditionalGeneration
 from flask_gtts import gtts
 import whisper
-import pickle
+import numpy as np
+from scipy.io import wavfile
+import pydub
 app = Flask(__name__)
 gtts(app)
 processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
 model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
 whis_model = whisper.load_model('tiny')
+import librosa
 
 # with open('blipmodel.pkl', 'rb') as f:
 #     model = pickle.load(f)
@@ -52,10 +54,11 @@ def blind():
 
 @app.route('/deaf', methods=['POST'])
 def deaf():
-    file = requests.files['audio']
-    audio = file.read()
-    text = whis_model.transcribe(audio)
-    return url_for(redirect('result_deaf.html', text=text))
+    file = request.files['audio']
+    file.save('static/audio.mp3')
+    y, sr = librosa.load('static/audio.mp3')
+    text = whis_model.transcribe(y)
+    return render_template('result_deaf.html', text=text['text'])
 
 @app.route('/mute', methods=['GET', 'POST'])
 def dumb():
